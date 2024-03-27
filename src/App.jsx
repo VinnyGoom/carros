fdd3ed979842bab2a35ffe4c34ccd177c7b8b065
Vinicius Gomes <Vini.goom@gmail.com>
2024-03-27 19:18:49 -0300

    terceiro commit do App

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
   return(
     <main>
       <section>
-        <div>
+        <div className="bg-orange">
           <img src={iconSedans} alt="icone dos carros sedan" />
           <h2>Sedans</h2>
           <p>Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
@@ -14,13 +14,13 @@ export default function App() {
 
 
         </div>
-        <div>
+        <div className="bg-green">
           <img src={iconSuvs} alt="Icone dos carros SUV" />
           <h2>SUVs</h2>
           <p>Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
           <a href="https://www.webmotors.com.br/suvs" target="_blank">Ver mais</a>
         </div>
-        <div>
+        <div className="bg-dark-green">
           <img src={iconLuxury} alt="Icone de carros luxuosos" />
           <h2>Luxuosos</h2>
           <p>Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
